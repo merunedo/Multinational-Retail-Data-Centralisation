499fb78a32af0109d1d58b232bf47643fe2d8057
merunedo <m.erunedo@gmail.com>
2023-12-30 01:58:45 +0000

    add features

--- data_extraction.py
+++ data_extraction.py
@@ -1,50 +1,53 @@
-# Import the required modules
-# requests for making HTTP requests to APIs
-# csv for handling CSV file operations
+# psycopg2 for connecting to PostgreSQL databases
+# yaml for parsing YAML files to read database credentials
+import psycopg2
+import yaml
 
-import requests
-import csv
-
-# Define a class named DataExtractor
 class DataExtractor:
 
-    # This is a special method called a constructor, which initializes the class
-    def __init__(self):
-        # You can initialize variables here if needed
-        pass
+    # Constructor that initializes the class with database configuration
+    def __init__(self, db_config_path):
+        # Load database credentials from the provided YAML file path
+        with open(db_config_path, 'r') as config_file:
+            self.db_config = yaml.safe_load(config_file)
+        # Establish a database connection
+        self.db_connection = self._connect_to_db()
 
-    # Method to extract data from a CSV file
-    def extract_from_csv(self, file_path):
-        # Create an empty list to store the data
-        data = []
-        # Open the CSV file
-        with open(file_path, mode='r', encoding='utf-8') as file:
-            # Use the csv.DictReader to read the CSV into a dictionary format
-            csv_reader = csv.DictReader(file)
-            # Loop through each row in the CSV
-            for row in csv_reader:
-                # Add each row to the data list
-                data.append(row)
-        # Return the list of data
-        return data
+    # Private method to establish connection to the database
+    def _connect_to_db(self):
+        # Connect to the database using credentials
+        return psycopg2.connect(
+            host=self.db_config['RDS_HOST'],
+            user=self.db_config['RDS_USER'],
+            password=self.db_config['RDS_PASSWORD'],
+            database=self.db_config['RDS_DATABASE'],
+            port=self.db_config['RDS_PORT']
+        )
 
-    # Method to extract data from an API
-    def extract_from_api(self, api_endpoint):
-        # Make a GET request to the API endpoint
-        response = requests.get(api_endpoint)
-        # Convert the response to JSON format
-        data = response.json()
-        # Return the data
-        return data
+    # Method to extract data from the database using a SQL query
+    def extract_from_db(self, query):
+        # Execute the query and return the results
+        with self.db_connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
-# Example usage of the class:
-# Create an instance of the DataExtractor class
-extractor = DataExtractor()
-# Call the method to extract data from a CSV file
-csv_data = extractor.extract_from_csv('path/to/your/file.csv')
-# Call the method to extract data from an API
-api_data = extractor.extract_from_api('https://api.yourservice.com/data')
+    # Method to close the database connection
+    def close_db_connection(self):
+        # Close the database connection if it's open
+        if self.db_connection:
+            self.db_connection.close()
+
+# Example usage:
+# Provide the path to your YAML file that contains database credentials
+db_config_path = 'db_creds.yaml'
+extractor = DataExtractor(db_config_path=db_config_path)
+
+# Here you will write your SQL query to extract data
+query = 'SELECT * FROM your_table_name'
+db_data = extractor.extract_from_db(query)
 
 # Print the data to see if it's working
-print(csv_data)
-print(api_data)
+print(db_data)
+
+# Don't forget to close the connection when done
+extractor.close_db_connection()
